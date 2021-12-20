50b6ff821f730e515a64c668cb723a9d5a0d485e
Sergey Isaev <stirlitz-s@yandex.ru>
2021-12-20 11:55:24 +0300

    Change getErrors and collection type

--- src/com/stsoft/runaerrors/Errors.java
+++ src/com/stsoft/runaerrors/Errors.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.function.Function;
 
 public class Errors {
+    
     private static Set<SystemError> systemErrors = new ConcurrentSkipListSet<>();
 
 
